989638e758e5ac668e509ac7a6db0900aa33a4fc
lgdelacruz <bboyjacks@gmail.com>
2019-11-14 14:17:33 -0800

    Updates

---
+++ precache-manifest.c5772eea1140574f6c720c60fe70f2ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd66200512c739f72fb0aa9f94f2d856",
+    "url": "/image-drag-and-scale/index.html"
+  },
+  {
+    "revision": "da181caf0832cb4ef7f1",
+    "url": "/image-drag-and-scale/static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "b3707255e17069c016bc",
+    "url": "/image-drag-and-scale/static/js/2.9a97152e.chunk.js"
+  },
+  {
+    "revision": "da181caf0832cb4ef7f1",
+    "url": "/image-drag-and-scale/static/js/main.ea61dafb.chunk.js"
+  },
+  {
+    "revision": "fc2b102b7ad970ff4525",
+    "url": "/image-drag-and-scale/static/js/runtime-main.3b643f8a.js"
+  }
+]);
